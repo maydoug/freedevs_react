943e61798c74aaef772c7e112e75ca9702547ede
unknown <maydoug@hotmail.com>
2022-07-02 13:06:14 -0300

    enviando dados para back via api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,56 @@
+import React, { useEffect, useState } from 'react'
 import './App.css';
+//import { api } from './api'
+// import axios from 'axios' ;
 
-function App() {
+function App() { 
+   //useEffect(() => {})
+
+   async function fetchPost(body){
+    fetch('https://api-freedevs.herokuapp.com/person', { 
+      method:'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body),
+  })
+  .then((response) => response.json())
+  .then((data) => alert('cadastro realizado com sucesso', data))
+  
+  console.log(body)
+  }
+
+   function envForm(e){
+      e.preventDefault() 
+
+      const dadosForm = {
+        name: document.getElementById('name').value,
+        sobrenome: document.getElementById('sobrenome').value,
+        email: document.getElementById('email').value,
+        phone: document.getElementById('phone').value,
+        app: document.getElementById('app').value,
+        pais: document.getElementById('pais').value,
+        descricao: document.getElementById('descricao').value
+      }
+
+      //const dados = JSON.stringify(dadosForm);
+
+      //console.log(dadosForm)
+      
+      fetchPost(dadosForm);
+
+
+    /* api.post('https://api-freedevs.herokuapp.com/person', { 
+      headers: {'Content-Type': 'application/json'},
+      method: 'POST',
+      data: JSON.stringify(dadosForm),
+      responseType: 'json'
+    })
+    .then((response) => console.log(response))
+    .catch((error) => (error))
+   */
+   }
+
   return (
     <div className="App">
       <div className="main">
@@ -16,25 +66,25 @@ function App() {
             </div>
           <div className="form">
             <h3>Encontre um desenvolvedor para seu projeto</h3>
-                 <form>
+                 <form onSubmit={envForm}>
                    <div className="items-form">
-                     <input type="text" placeholder="SEU NOME"/>
-                     <input type="text" placeholder="SOBRENOME"/>
-                     <input type="text" placeholder="DIGITE SEU E-MAIL"/>
-                     <input type="text" placeholder="TELEFONE"/>
-                     <select id="typeProject">
+                     <input type="text" id="name" name="name" placeholder="SEU NOME"/>
+                     <input type="text" id="sobrenome" name="nome" placeholder="SOBRENOME"/>
+                     <input type="text" id="email" name="email" placeholder="DIGITE SEU E-MAIL"/>
+                     <input type="text" id="phone" name="phone" placeholder="TELEFONE"/>
+                     <select id="app" name="app">
                        <option value="webSite">Web Site</option>
                        <option value="app">Aplicativo</option>
                        <option value="ladingPage">Lading Page</option>
                        <option value="API">API</option>
                        <option value="API">Game</option>
                      </select>
-                     <select id="pais">
+                     <select id="pais" name="pais">
                        <option value="brasil">Brasil</option>
                        <option value="canada">Canada</option>
                        <option value="USA">USA</option>
                      </select>
-                     <textarea id="texto" placeholder="O QUE VAMOS CRIAR?" name="projeto"/>
+                     <textarea id="descricao" name="descricao" placeholder="O QUE VAMOS CRIAR?"/>
                      <input type="submit" />
                    </div>
                   </form> 
